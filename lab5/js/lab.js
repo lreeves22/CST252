06203438c3887c6b27d0266e5bc98651f37a3edb
Lauren Reeves <lreeves@csumb.edu>
2021-09-15 14:52:06 -0700

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,32 @@
+/**
+ * Summary. (use period)
+ *
+ * Description. Lab 5 -Data Types and Variables
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author Lauren Reeves.
+ * @since  x.x.x
+ */
+ // Declare variables to hold the make, model, color, and year of your vehicle,
+// example, var year= 2012
+ var make = "Subaru"
+ var model = "Forester"
+ var color = "Blue"
+ var year = "2012"
+ var extras = "sun roof, surf racks, cool stickers on back window"
+
+
+ //  Declare a boolean variable ownIt for whether you own it or not.
+ var ownIt = True;
+
+ //  Create a new variable age that subtracts the year from the current year
+ var age = new Date().getFullYear();
+
+ //  Use document.writeln() to neatly output each of your variables, examples
+ // document.writeln("Make: " + make + "<br>");
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Extras: " + extras + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + "<br>");
+ 
