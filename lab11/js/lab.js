6de4d0e7a8f340f1d13d2c8b5c3685292cea54dd
Lauren Reeves <lreeves@csumb.edu>
2021-10-13 12:49:58 -0700

    lab 12

---
+++ lab11/js/lab.js
@@ -0,0 +1,5 @@
+/**
+ * Author:    Lauren Reeves
+ * Created:   10/11/2021
+ * Class: CST 252
+ **/
---
+++ lab12/js/lab.js
@@ -0,0 +1,17 @@
+/**
+ * Author:    Lauren Reeves
+ * Created:   10/13/2021
+ * Class: CST 252
+ **/
+ 
+ $("#summary-button").click(function(){
+	$("#first").toggleClass("kickflip");
+})
+
+$("#difficulties-button").click(function(){
+	$("#second").toggleClass("heelflip");
+})
+
+$("#results-button").click(function(){
+	$("#third").toggleClass("varial");
+})
